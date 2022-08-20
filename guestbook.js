0025f51331c9d9fad7f3b53c9a42196a5e0a617b
William M Cornejo <42680104+willm625@users.noreply.github.com>
2022-08-20 10:30:04 -0400

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://a8962ab4.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
